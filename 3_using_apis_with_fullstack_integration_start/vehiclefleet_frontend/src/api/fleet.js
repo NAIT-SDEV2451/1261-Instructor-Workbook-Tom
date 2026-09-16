4281e7e66e7f522396f818244637487fed549095
Tom Maurer <tmaurer@nait.ca>
2026-09-16 09:09:48 -0600

    api integration pt 1

---
+++ 3_using_apis_with_fullstack_integration_start/vehiclefleet_frontend/src/api/fleet.js
@@ -0,0 +1,9 @@
+const BASE_URL = "http://localhost:8000/api/v1";
+
+export async function fetchVehicles() {}
+
+export async function fetchDrivers() {}
+
+export async function fetchTrips() {}
+
+export async function createTrip(data) {}
